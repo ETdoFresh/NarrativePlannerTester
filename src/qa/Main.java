e4c5519e7c0ea6bdfde869c8f4c1e9958c88844d
ETdoFresh <etdofresh@gmail.com>
2019-03-10 16:33:21 -0500

    Open file with default program (not always notepad). Should [Info] in front of none Pass/Fail/Warn tests.

--- src/qa/Main.java
+++ src/qa/Main.java
@@ -1,7 +1,9 @@
 package qa;
 
+import java.awt.Desktop;
 import java.io.File;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 
 import sabre.*;
 import sabre.graph.PlanGraph;
@@ -26,40 +28,60 @@ public class Main {
 			+ "\n using the Sabre Narrative Planner v0.32 by Stephen G. Ware";
 	private static final String USAGE = "... Sit back, relax, and enjoy the demo ...\n";
 	private static final String FILE = "sample.txt";
-	
+
 	private static final String PASS = "[" + TextColor.GREEN + "Pass" + TextColor.RESET + "] ";
 	private static final String FAIL = "[" + TextColor.RED + "Fail" + TextColor.RESET + "] ";
 	private static final String WARN = "[" + TextColor.YELLOW + "Warn" + TextColor.RESET + "] ";
-	
+	private static final String INFO = "[" + TextColor.BLUE + "Info" + TextColor.RESET + "] ";
+
 	private static final String SYNTAX = "File is syntactically correct";
 	private static final String GOAL = "Goal specified";
 	private static final String INITIAL = "Goal not true in initial state";
 	private static final String SOLUTION = "Solution exists";
 
 	public static void main(String[] args) {
-		
-		// Open domain file in notepad
-		Runtime runtime = Runtime.getRuntime();
+
+		File file = new File(FILE);
+
+		// Open domain file
+		Desktop desktop = Desktop.getDesktop();
 		try {
-			Process process = runtime.exec("notepad " + FILE);
+			file.createNewFile(); // does nothing if file exists
+			desktop.open(file);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		File file = new File(FILE);
 		long lastModified = 0;
 
+		// Clear/Reset Screen
+		System.out.flush();
+		System.out.println(TITLE);
+		System.out.println(USAGE);
+
+		boolean firstRun = true;
 		while (true) {
-			if (lastModified == file.lastModified())
+			if (lastModified == file.lastModified()) {
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 				continue;
+			}
 
 			// Update last modified
 			lastModified = file.lastModified();
 
-			// Clear/Reset Screen
-		    System.out.flush();
-			System.out.println(TITLE);
-			System.out.println(USAGE);
-			
+			if (firstRun) {
+				firstRun = false;
+				System.out.println(INFO + "File Opened: " + FILE + " Last Modified: " + new SimpleDateFormat("MM/dd/yyyy HH:mm:ss").format(lastModified));
+			} else {
+				System.out.println();
+				System.out.println();
+				System.out.println(INFO + "File Modified: " + FILE + " Last Modified: " + new SimpleDateFormat("MM/dd/yyyy HH:mm:ss").format(lastModified));
+			}
+
 			// Parse Domain
 			Parser parser = new DefaultParser();
 			Domain domain;
@@ -73,40 +95,41 @@ public class Main {
 
 			// Space
 			SearchSpace space = Utilities.get(status -> new SearchSpace(domain, status));
-			System.out.println("Number of ground actions: " + space.actions.size());
+			System.out.println(INFO + "Number of ground actions: " + space.actions.size());
 
 			// Check if goal is empty
 			if (domain.goal.equals(Expression.TRUE))
 				System.out.println(FAIL + GOAL);
 			else
 				System.out.println(PASS + GOAL);
-			
-			// Check if initial state equals goal state <-- doesn't make sense bc goal is not a state
+
+			// Check if initial state equals goal state <-- doesn't make sense bc goal is
+			// not a state
 //			if (domain.initial.equals(domain.goal))
 //				System.out.println(WARN + "Initial State == Goal State");
 //			else
 //				System.out.println(PASS + "Initial State == Goal State");
-						
+
 			// Check if goal is true in initial state
 			ArrayState initial = new ArrayState(space);
-			if(domain.goal.test(initial))
+			if (domain.goal.test(initial))
 				System.out.println(FAIL + INITIAL);
 			else
 				System.out.println(PASS + INITIAL);
-			
+
 			// Plan Graph
 			space.graph.initialize(initial);
-			while(!space.graph.hasLeveledOff())
+			while (!space.graph.hasLeveledOff())
 				space.graph.extend(); // Extend graph until all goals have appeared
-			System.out.println("Size of plan graph: " + space.graph.size());
-			
+			System.out.println(INFO + "Size of plan graph: " + space.graph.size());
+
 			// Number of actions available from the initial state
 			int firstSteps = 0;
-			for(Action action : space.actions)
-				if(action.precondition.test(initial))
+			for (Action action : space.actions)
+				if (action.precondition.test(initial))
 					firstSteps++;
-			System.out.println("Number of actions available in initial state: " + firstSteps);
-			
+			System.out.println(INFO + "Number of actions available in initial state: " + firstSteps);
+
 			// Check if a solution exists
 			Planner planner = new Planner();
 			planner.setSearchSpace(space);
@@ -114,19 +137,17 @@ public class Main {
 			RootNode root = new RootNode(initial);
 			search.push(root);
 			Result result = Utilities.get(status -> search.getNextSolution(status));
-			if(result.plan != null)
+			if (result.plan != null)
 				System.out.println(PASS + SOLUTION);
 			else
 				System.out.println(FAIL + SOLUTION);
-			
+
 			// Find all solutions
-/*			while(result.plan != null) {
-				System.out.println(result);
-				for (Action action : result.plan)
-					System.out.println(action);
-				result = Utilities.get(status -> search.getNextSolution(status));
-			}
-*/			
+			/*
+			 * while(result.plan != null) { System.out.println(result); for (Action action :
+			 * result.plan) System.out.println(action); result = Utilities.get(status ->
+			 * search.getNextSolution(status)); }
+			 */
 		}
 	}
 }
