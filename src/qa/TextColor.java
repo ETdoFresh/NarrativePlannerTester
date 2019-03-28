318c3519819bc8fe85c0f8e7d0c46bc55cb57785
ETdoFresh <ETdoFresh@gmail.com>
2019-03-28 17:38:30 -0500

    Remove text color from output (can add back easily)

--- src/qa/TextColor.java
+++ src/qa/TextColor.java
@@ -1,23 +1,26 @@
 package qa;
 
 public final class TextColor {
-	public static final String RESET = "\u001B[0m";
 	
-	public static final String BLACK = "\u001B[30m";
-	public static final String RED = "\u001B[1;31m";
-	public static final String GREEN = "\u001B[32m";
-	public static final String YELLOW = "\u001B[33m";
-	public static final String BLUE = "\u001B[34m";
-	public static final String PURPLE = "\u001B[35m";
-	public static final String CYAN = "\u001B[36m";
-	public static final String WHITE = "\u001B[37m";
+	private static final boolean SHOW_COLOR = false
+			;
+	public static final String RESET = SHOW_COLOR ? "\u001B[0m" : "";
 	
-	public static final String BLACK_BACKGROUND = "\u001B[40m";
-	public static final String RED_BACKGROUND = "\u001B[41m";
-	public static final String GREEN_BACKGROUND = "\u001B[42m";
-	public static final String YELLOW_BACKGROUND = "\u001B[43m";
-	public static final String BLUE_BACKGROUND = "\u001B[44m";
-	public static final String PURPLE_BACKGROUND = "\u001B[45m";
-	public static final String CYAN_BACKGROUND = "\u001B[46m";
-	public static final String WHITE_BACKGROUND = "\u001B[47m";
+	public static final String BLACK = SHOW_COLOR ? "\u001B[30m" : "";
+	public static final String RED = SHOW_COLOR ? "\u001B[1;31m" : "";
+	public static final String GREEN = SHOW_COLOR ? "\u001B[32m" : "";
+	public static final String YELLOW = SHOW_COLOR ? "\u001B[33m" : "";
+	public static final String BLUE = SHOW_COLOR ? "\u001B[34m" : "";
+	public static final String PURPLE = SHOW_COLOR ? "\u001B[35m" : "";
+	public static final String CYAN = SHOW_COLOR ? "\u001B[36m" : "";
+	public static final String WHITE = SHOW_COLOR ? "\u001B[37m" : "";
+	
+	public static final String BLACK_BACKGROUND = SHOW_COLOR ? "\u001B[40m" : "";
+	public static final String RED_BACKGROUND = SHOW_COLOR ? "\u001B[41m" : "";
+	public static final String GREEN_BACKGROUND = SHOW_COLOR ? "\u001B[42m" : "";
+	public static final String YELLOW_BACKGROUND = SHOW_COLOR ? "\u001B[43m" : "";
+	public static final String BLUE_BACKGROUND = SHOW_COLOR ? "\u001B[44m" : "";
+	public static final String PURPLE_BACKGROUND = SHOW_COLOR ? "\u001B[45m" : "";
+	public static final String CYAN_BACKGROUND = SHOW_COLOR ? "\u001B[46m" : "";
+	public static final String WHITE_BACKGROUND = SHOW_COLOR ? "\u001B[47m" : "";
 }
