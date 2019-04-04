f28901e0f888c593d324f580e7e4c863381aec11
ETdoFresh <ETdoFresh@gmail.com>
2019-04-04 14:41:49 -0500

    Remove filters that were implemented before Explanation.

--- src/qa/RelaxedPlanExtractor.java
+++ src/qa/RelaxedPlanExtractor.java
@@ -46,17 +46,7 @@ public class RelaxedPlanExtractor {
 	static Collection<RelaxedPlan> GetAllPossiblePlanGraphPlans(ArrayList<RelaxedPlan> plans, RelaxedPlan plan,
 			ArrayList<PlanGraphLiteralNode> localGoalLiterals, ArrayList<PlanGraphLiteralNode> initialGoalLiterals,
 			ArrayList<Explanation> explanations) {
-
-		// localGoalLiterals - goals to be found on this iteration
-		// initialGoalLiterals - initial goal literals specified at the start
-
-		// Determine which initial goal literals have been found already
-		ArrayList<PlanGraphLiteralNode> foundGoalLiterals = new ArrayList<>();
-		foundGoalLiterals.addAll(initialGoalLiterals);
-		for (int i = foundGoalLiterals.size() - 1; i >= 0; i--)
-			if (localGoalLiterals.contains(foundGoalLiterals.get(i)))
-				foundGoalLiterals.remove(i);
-
+		
 		// Remove Initial State Literals from GoalLiterals
 		for (int i = localGoalLiterals.size() - 1; i >= 0; i--) {
 			PlanGraphLiteralNode goalLiteral = localGoalLiterals.get(i);
@@ -73,15 +63,6 @@ public class RelaxedPlanExtractor {
 		for (PlanGraphLiteralNode goalLiteral : localGoalLiterals) {
 			for (PlanGraphNode actionNode : goalLiteral.parents) {
 				PlanGraphActionNode action = (PlanGraphActionNode) actionNode;
-				int min = action.graph.size();
-				for (PlanGraphActionNode node : plan)
-					if (node.getLevel() < min)
-						min = node.getLevel();
-
-				// Due to relaxed nature, do not use same ground action twice
-				// Do not grab actions beyond current level (min)
-				if (action.getLevel() > min || plan.contains(action))
-					continue;
 
 				ArrayList<PlanGraphLiteralNode> newGoalLiterals = new ArrayList<>(localGoalLiterals);
 				newGoalLiterals.remove(goalLiteral);
@@ -90,42 +71,38 @@ public class RelaxedPlanExtractor {
 				for (Literal newLiteral : newLiterals)
 					newGoalLiterals.add(action.graph.getLiteral(newLiteral));
 
-				// Skip if this finds the goal again/earlier
-				boolean foundAlreadyReachedGoalLiteral = false;
-				for (PlanGraphLiteralNode foundGoalLiteral : foundGoalLiterals)
-					if (newGoalLiterals.contains(foundGoalLiteral)) {
-						foundAlreadyReachedGoalLiteral = true;
-						break;
-					}
-
-				if (foundAlreadyReachedGoalLiteral)
-					continue;
-
 				if (!canBeExplainedByAtLeastOneConsentingCharacter(action, explanations))
 					continue;
 
 				RelaxedPlan planWithNewAction = plan.clone();
 				planWithNewAction.push(action);
 
-				ArrayList<Explanation> newExplanations = new ArrayList<>();
-				for (Explanation explanation : explanations) {
-					if (explanation.containsEffect(action.event)) {
-						Explanation newExplanation = explanation.clone();
-						newExplanation.applyAction(action.event);
-						newExplanations.add(newExplanation);
-					} else
-						newExplanations.add(explanation);
-
-				}
+				ArrayList<Explanation> newExplanations = cloneExplanation(explanations, action);
 
 				Collection<RelaxedPlan> newPlan = GetAllPossiblePlanGraphPlans(plans, planWithNewAction,
 						newGoalLiterals, initialGoalLiterals, newExplanations);
+				
 				if (newPlan != plans)
 					plans.addAll(newPlan);
 			}
 		}
 
 		return plans;
+	}
+
+	private static ArrayList<Explanation> cloneExplanation(ArrayList<Explanation> explanations,
+			PlanGraphActionNode action) {
+		ArrayList<Explanation> newExplanations = new ArrayList<>();
+		for (Explanation explanation : explanations) {
+			if (explanation.containsEffect(action.event)) {
+				Explanation newExplanation = explanation.clone();
+				newExplanation.applyAction(action.event);
+				newExplanations.add(newExplanation);
+			} else
+				newExplanations.add(explanation);
+
+		}
+		return newExplanations;
 	}
 
 	private static boolean canBeExplainedByAtLeastOneConsentingCharacter(PlanGraphActionNode actionNode,
